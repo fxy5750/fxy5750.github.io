1a85befaa824546c8c9daee4cbe0711afccacd07
fxy5750 <fxy5750@163.com>
2024-04-03 18:40:17 +0800

    Site updated: 2024-04-03 18:40:15

--- Resume/assets/js/index.js
+++ Resume/assets/js/index.js
@@ -1,1 +1,4 @@
 console.log("感谢您的浏览，期待能够加入贵公司！我的网站：https://fxy5750.github.io/");
+console.log('%cCopyright © 2024 fxy5750.github.io',
+    'background-color: #ff00ff; color: white; font-size: 24px; font-weight: bold; padding: 10px;'
+);
