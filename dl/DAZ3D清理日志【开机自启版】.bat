a477132c9c3b5ce48660b8b3d0ff9a890a2f0260
fxy5750 <fxy5750@163.com>
2024-12-27 14:27:12 +0800

    Site updated: 2024-12-27 14:27:08

---
+++ dl/DAZ3D清理日志【开机自启版】.bat
@@ -0,0 +1,10 @@
+@ECHO OFF
+TITLE %device_name%     By：xiaoxiao
+
+ECHO. 【删除 log.txt & dazstudio.dau】
+del "%AppData%\DAZ 3D\Studio4\log*.txt" 
+del "%AppData%\DAZ 3D\Studio4\dazstudio.dau" 
+ECHO. 复制【清理DAZ3D日志】作为开机自启计划
+copy DAZ3D清理日志【开机自启版】.bat "%USERPROFILE%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\"  
+
+  
